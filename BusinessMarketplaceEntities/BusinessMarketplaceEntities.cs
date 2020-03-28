c0bcd3d03ac4056ff8a6c9908330f0b55ac85075
Fershark <ferchriquelme@gmail.com>
2020-03-28 02:17:35 -0700

    Db Model

--- BusinessMarketplaceEntities/BusinessMarketplaceEntities.cs
+++ BusinessMarketplaceEntities/BusinessMarketplaceEntities.cs
@@ -1,12 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BusinessMarketplaceEntities
 {
-    public class BusinessMarketplaceEntities
+    public class BusinessMarketplaceEntities: DbContext
     {
+
+        public BusinessMarketplaceEntities(): base("name=BusinessMarketplaceConnection")
+        {
+        }
+
+        public virtual DbSet<Business> Businesses { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Business>()
+                .HasMany(e => e.Products)
+                .WithRequired(e => e.Business)
+                .HasForeignKey(e => e.BusinessId)
+                .WillCascadeOnDelete(true);
+
+            modelBuilder.Entity<Business>()
+                .HasMany(e => e.Orders)
+                .WithRequired(e => e.Business)
+                .HasForeignKey(e => e.BusinessId)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Category>()
+                .HasMany(e => e.Products)
+                .WithRequired(e => e.Category)
+                .HasForeignKey(e => e.CategoryId)
+                .WillCascadeOnDelete(true);
+
+            modelBuilder.Entity<Order>()
+                .HasMany(e => e.Products)
+                .WithMany(e => e.Orders)
+                .Map(po =>
+                {
+                    po.MapLeftKey("OrderId");
+                    po.MapRightKey("ProductId");
+                    po.ToTable("OrderProducts");
+                });
+        }
     }
 }
---
+++ BusinessMarketplaceEntities/EnittiesClasses.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessMarketplaceEntities
+{
+    [Table("Businesses")]
+    public partial class Business
+    {
+        public Business()
+        {
+            Products = new HashSet<Product>();
+            Orders = new HashSet<Order>();
+        }
+
+        [DisplayName("Business ID")]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [DisplayName("Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [DisplayName("Address")]
+        public string Address { get; set; }
+
+        [Required]
+        [Index(IsUnique = true)]
+        [StringLength(50)]
+        [DisplayName("Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DisplayName("Is Admin")]
+        public bool IsAdmin { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+        public virtual ICollection<Order> Orders { get; set; }
+
+    }
+
+    [Table("Products")]
+    public partial class Product
+    {
+        public Product()
+        {
+            Orders = new HashSet<Order>();
+        }
+
+        [DisplayName("Product ID")]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [DisplayName("Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [DisplayName("Description")]
+        public string Description { get; set; }
+
+        [Required]
+        [DisplayName("QuantityInStock")]
+        public int QuantityInStock { get; set; }
+
+        [Required]
+        [DisplayName("Price")]
+        public decimal Price { get; set; }
+
+        [Required]
+        [DisplayName("Business ID")]
+        [ForeignKey("Business")]
+        public int BusinessId { get; set; }
+        public virtual Business Business { get; set; }
+
+        [Required]
+        [DisplayName("Category ID")]
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+
+    [Table("Categories")]
+    public partial class Category
+    {
+        public Category()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        [DisplayName("Category ID")]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [DisplayName("Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [DisplayName("Description")]
+        public string Description { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+
+    [Table("Orders")]
+    public partial class Order
+    {
+        public Order()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        [DisplayName("Order ID")]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("Order Date")]
+        public DateTime OrderDate { get; set; }
+
+        [Required]
+        [DisplayName("Total Price")]
+        public decimal TotalPrice { get; set; }
+
+        [Required]
+        [DisplayName("Business ID")]
+        [ForeignKey("Business")]
+        public int BusinessId { get; set; }
+        public virtual Business Business { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+
+    }
+}
