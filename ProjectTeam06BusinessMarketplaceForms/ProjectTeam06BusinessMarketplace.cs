2488df7e6e8854992dfb2959f31bda037964a689
Fershark <ferchriquelme@gmail.com>
2020-03-27 21:58:04 -0700

    Project creation with naming conventions

---
+++ ProjectTeam06BusinessMarketplaceForms/ProjectTeam06BusinessMarketplace.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectTeam06BusinessMarketplaceForms
+{
+    static class ProjectTeam06BusinessMarketplace
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new ProjectTeam06BusinessMarketplaceForm());
+        }
+    }
+}
